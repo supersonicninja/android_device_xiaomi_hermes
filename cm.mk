0c914e4aeda988d2e8672d21b5bb6b741f039565
jianminglok <jianminglok@hotmail.com>
2016-02-11 11:22:44 +0800

    Update More

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/xiaomi/hermes/full_hermes.mk)
+$(call inherit-product, device/xiaomi/hermes/device_hermes.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
