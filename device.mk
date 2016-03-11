757351053475afd34f758ba3e1865fd08b1fc44b
supersonicninja <xdaninja@yahoo.co.jp>
2016-03-11 05:35:09 -0800

    apn : switch to global list
    Change-Id: Iaf23569c9e908ab0a8dcb8343dfc3af24705f02a

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_COPY_FILES += \
 
 # Telecom
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml \
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
     $(LOCAL_PATH)/configs/ecc_list.xml:system/etc/ecc_list.xml \
     $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml 
 
