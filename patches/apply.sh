611eda8a1e1b7595b13ad249d4b491351501dfa7
supersonicninja <xdaninja@yahoo.co.jp>
2016-03-11 05:01:19 -0800

    patches : clean up
    Remove unused lines from apply.sh.
    Remove deleting log features from 0001-fix-boot-for-mtk.patch for compatibility with android5.1.1-r35.
    Remove already applied parts by official cyanogenmod from 0001-fix-gps-lights-audio.patch and rename it.
    Add gitignore to ignore ~ files.
    
    Change-Id: I190b3d17eb8473c5534ae0bbe76d099e53b97ea7

--- patches/apply.sh
+++ patches/apply.sh
@@ -7,8 +7,6 @@ cd frameworks/base
 git apply -v ../../device/xiaomi/hermes/patches/framework_base/0001-location-support-GNSS-extension-presumably-from-MTK.patch
 git apply -v ../../device/xiaomi/hermes/patches/framework_base/0002-location-add-some-logs-just-to-be-confident.patch
 git apply -v ../../device/xiaomi/hermes/patches/framework_base/0003-location-style-fix.patch
-git apply -v ../../device/xiaomi/hermes/patches/framework_base/0004-NetworkMonitor-use-g.cn-for-connectivity-check.patch
-git apply -v ../../device/xiaomi/hermes/patches/framework_base/0005-SystemUI-add-new-carrier-name-override-rule-for-Chin.patch
 git apply -v ../../device/xiaomi/hermes/patches/framework_base/0006-add-com.mediatek-for-build.patch
 git apply -v ../../device/xiaomi/hermes/patches/framework_base/0007-Update-for-mtk.patch
 cd ../..
@@ -25,7 +23,7 @@ cd system/netd
 git apply -v ../../device/xiaomi/hermes/patches/system_netd/0001-hotpost-fix.patch
 cd ../..
 cd hardware/libhardware
-git apply -v ../../device/xiaomi/hermes/patches/hardware_libhardware/0001-fix-gps-light-audio.patch
+git apply -v ../../device/xiaomi/hermes/patches/hardware_libhardware/0001-fix-gps-audio.patch
 cd ../libhardware_legacy
 git apply -v ../../device/xiaomi/hermes/patches/hardware_libhardware_legacy/0001-patch-for-mtk.patch
 cd ../..
